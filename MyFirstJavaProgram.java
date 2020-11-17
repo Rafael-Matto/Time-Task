c533d4f4ccda81fd9ccea12e39519a3a701898a2
Thayene Lorens <thayenelorens@gmail.com>
2020-11-17 14:09:15 +0000

    Update MyFirstJavaProgram.java

--- MyFirstJavaProgram.java
+++ MyFirstJavaProgram.java
@@ -16,6 +16,7 @@ public class MyFirstJavaProgram {
      */
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Hi, Thayene here");
     }
     
 }

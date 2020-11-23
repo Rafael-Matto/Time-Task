9d5cade54915ad34c78f4bdc5d6608af1bb8c93e
Rafael-Matto <72030700+Rafael-Matto@users.noreply.github.com>
2020-11-23 22:41:41 +0000

    Add files via upload

--- myMethod/myMethod/src/mymethod/MyMethod.java
+++ myMethod/myMethod/src/mymethod/MyMethod.java
@@ -6,11 +6,18 @@
 package mymethod;
 
 import java.util.Scanner;
+import static mymethod.boyfriend.Nationality;
+import static mymethod.boyfriend.closure;
 import static mymethod.boyfriend.introduction;
+import static mymethod.boyfriend.status;
 
 /**
  *
- * @author Thay
+ * @author Group s - Shamrock
+ * Participants:
+ * -Thayene Lorens (student number : 2020293)
+ * -Marco Antônio dos Santos (student number : 2020333)
+ * -Rafael Matos (student number: 2020345)
  */
 public class MyMethod {
 
@@ -18,27 +25,59 @@ public class MyMethod {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       
-        introduction();
-        Scanner input = new Scanner(System.in);
-        boyfriend bf = new boyfriend();
-        System.out.println("Enter the name of first boyfriend here: ");
+      
+           //Calling this method to input the intoductory sentence of this program.
+           introduction(); 
         
-        String temp = input.nextLine(); //The name we type is stored in the variable temp.
+           //Using Scanner for input.
+           Scanner input = new Scanner(System.in); 
         
-        bf.setName(temp); //
+           //Code to make it possible to access the 'boyfriend' class.
+           boyfriend bf = new boyfriend();
         
-        bf.saying();// boy name = X. Uses the saying.
-                
-        boyfriend age = new boyfriend();
+           //Basic prompt to tell the user what to do.
+           System.out.println("Enter the name of first boyfriend here: "); 
         
-        System.out.println("What was his age?");
+           //The name we type is stored in the variable 'word'.
+           String word = input.nextLine(); 
         
-        int answer = input.nextInt();
+    /*loop in case user inputs number or other digits instead of just letters. 
+     The loop ends when user types letters only.*/
+    while(!word.matches("[a-zA-Z- ',]+")){
         
-        age.setAge(answer);
-        Nationality();
-
+           System.out.println("His name has to be text only.\nEnter the name of first boyfriend here: \n");
+                      
+           word = input.nextLine();
+           
+       }
+           //It gets the word that was input and set it equals to boyName.
+           bf.setName(word); 
+           
+           //Calling this method so the sentence can be output.
+           bf.sentence();
+           //Calling the method getName.
+           bf.getName();
+         
+    try{
+          
+           boyfriend age = new boyfriend();
+        
+           System.out.println("What was his age?");
+        
+           int answer = input.nextInt();
+           
+           //Calling the method 'setAge'.
+           age.setAge(answer);
     }
+    catch (Exception e){
+           
+           System.out.println("His age has to be a number only.");
+    }
+           Nationality ();
+           status();
+           closure();
+       
+     }
+    } 
+
     
-}
--- myMethod/myMethod/src/mymethod/boyfriend.java
+++ myMethod/myMethod/src/mymethod/boyfriend.java
@@ -5,57 +5,98 @@
  */
 package mymethod;
 
+import java.util.Scanner;
+
 /**
- *
- * @author Thayene Lorens
+ * @author Group s - Shamrock
+ * Participants:
+ * -Thayene Lorens (student number : 2020293)
+ * -Marco Antônio dos Santos (student number : 2020333)
+ * -Rafael Matos (student number: 2020345)
  */
 public class boyfriend {
        
     String boyName;
     
     public void setName (String name){
+    
+    /**
+     * author: Thayene Lorens
+     * Student Number: 2020293
+     */
         
     boyName = name;
     }
     
     public String getName (){
+         
+    /**
+     * author: Thayene Lorens
+     * Student Number: 2020293
+     */
         
     return boyName;
     }
-    public void saying(){
+   
+    public void sentence(){
+         
+    /**
+     * author: Thayene Lorens
+     * Student Number: 2020293
+     */
         
         System.out.println("Your first boyfriend's name was " + getName() + ".\n");
     }
     public int setAge(int age){
         
-        if (age >= 0 && age < 18){
+    /**
+     * author: Thayene Lorens
+     * Student Number: 2020293
+     */
+               
+    if (age >= 0 && age < 18){
             
-            System.out.println("OMG! He was too young to date you.");
-        }
-        else if(age == 18){
-            System.out.println("He was your age!");
-        }
-        else if (age < 0) {
-         System.out.println("That is impossible.");
-        } 
-        else if(age > 18 && age <= 25){
-            
-            System.out.println("Ideal");
-        }
-        else if (age >= 26 && age <=100){
-            
-            System.out.println("Too old for you");
-        }
-        else {
-            System.out.println("That ain't possible");
-        }
-        return age;
+        System.out.println("OMG! He was too young to date, still a babe!\n");
     }
+        
+    else if(age == 18){
+            
+        System.out.println("He was about your age!\n");
+    }
+        
+    else if (age < 0) {
+         
+        System.out.println("That is impossible. Liar! He doesn't exist!\n");
+    } 
+        
+    else if(age > 18 && age <= 25){
+            
+        System.out.println("Great! He was marriage material!\n");
+    }
+        
+    else if (age >= 26 && age <=34){
+            
+        System.out.println("He was a bit old for you\n");
+    }    
+        
+    else if (age >= 35 && age <=100){
+            
+        System.out.println("He was a sugar daddy!\n");
+    }
+        
+    else {
+            
+        System.out.println("That is unlikely to have happened. Were you dating a mummy?\n");
+    }                   
+        
+    return age;
+    }
+        
    public static void introduction(){
        
        System.out.println("Please, tell us the story of your very first relationship.\n");
    }
-    static void Nationality (){
+   static void Nationality (){
        
        /**
         * @author: Rafael Matos
@@ -70,12 +111,47 @@ public class boyfriend {
         country = myKB.nextLine();
         
         if (country.matches("[a-zA-Z]+")){
-            System.out.println("He is from " +country);
+            System.out.println("He is from " + country + ".\n");
         }
         else{
-            System.out.println("This is not a country!");
+            System.out.println("This is not a country.");
         }
       }catch (Exception e){
        }
+     }
+    private boolean inputhasNextLine() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    static int status() {
+        // TODO code application logic here
+        //String status;
+        Scanner mkb = new Scanner(System.in);
+        
+        System.out.println("What is your marital status? please choose: 1 single, 2 married, 3 divorced and 4 if you prefer not to say");
+        int status = mkb.nextInt();
+        
+      
+        switch(status){
+            case 1:
+                System.out.println("Single\n");   
+            break;
+            case 2:
+                System.out.println("Married\n");   
+            break;
+            case 3:
+                System.out.println("Divorced\n");   
+            break;
+            default:
+                System.out.println("I prefer not to say\n");  
+                
+           // return age; 
+        
+        } 
+         return 0;
+     }
+     public static void closure(){
+       
+       System.out.println("And that's a little bit of my story...\nTo be continued!");
+   }
 }
-}
+   

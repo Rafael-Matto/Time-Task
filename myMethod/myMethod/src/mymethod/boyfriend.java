f6ce6439693d388d049c9c0881beeff05efe16ce
Thayene Lorens <thayenelorens@gmail.com>
2020-11-24 10:52:27 +0000

    Deleting to make some changes

--- myMethod/myMethod/src/mymethod/boyfriend.java
+++ myMethod/myMethod/src/mymethod/boyfriend.java
@@ -17,91 +17,7 @@ import java.text.DecimalFormat;
  */
 public class boyfriend {
          
-    String boyName;
     
-    public void setName (String name){
-    
-    /**
-     * author: Thayene Lorens
-     * Student Number: 2020293
-     */
-        
-    boyName = name;
-    }
-    
-    public String getName (){
-         
-    /**
-     * author: Thayene Lorens
-     * Student Number: 2020293
-     */
-        
-    return boyName;
-    }
-   
-    public void sentence(){
-         
-    /**
-     * author: Thayene Lorens
-     * Student Number: 2020293
-     */
-        
-        System.out.println("Your first boyfriend's name was " + getName() + ".\n");
-    }
-    public int setAge(int age){
-        
-    /**
-     * author: Thayene Lorens
-     * Student Number: 2020293
-     */
-               
-    if (age >= 0 && age < 18){
-            
-        System.out.println("OMG! He was too young to date, still a babe!\n");
-    }
-        
-    else if(age == 18){
-            
-        System.out.println("He was about your age!\n");
-    }
-        
-    else if (age < 0) {
-         
-        System.out.println("That is impossible. Liar! He doesn't exist!\n");
-    } 
-        
-    else if(age > 18 && age <= 25){
-            
-        System.out.println("Great! He was marriage material!\n");
-    }
-        
-    else if (age >= 26 && age <=34){
-            
-        System.out.println("He was a bit old for you\n");
-    }    
-        
-    else if (age >= 35 && age <=100){
-            
-        System.out.println("He was a sugar daddy!\n");
-    }
-        
-    else {
-            
-        System.out.println("That is unlikely to have happened. Were you dating a mummy?\n");
-    }                   
-        
-    return age;
-    }
-        
-   public static void introduction(){
-
-    /**
-     * author: Thayene Lorens
-     * Student Number: 2020293
-     */
-       
-       System.out.println("Please, tell us the story of your very first relationship.\n");
-   }
     static void Nationality (){
        
        /**
@@ -156,14 +72,6 @@ public class boyfriend {
         } 
          return 0;
      }
-     public static void closure(){
-    
-    /**
-     * author: Thayene Lorens
-     * Student Number: 2020293
-     */
-       
-       System.out.println("And that's a little bit of my story...\nTo be continued!");
-   }
+  
        }
      }
